7837de738b062d8d33e1a168ebccaaa1cd419787
mishima2077 <mertergorun@hotmail.com>
2026-04-13 14:45:50 +0300

    Updated CMakeList.txt

--- shaders/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-glslc "$SCRIPT_DIR/shader.vert" -o "$SCRIPT_DIR/vert.spv"
-glslc "$SCRIPT_DIR/shader.frag" -o "$SCRIPT_DIR/frag.spv"
-echo "Shaders compiled successfully"
